49d847d4bde5085db92ae33d507f0a7c018872ae
Kris Powell <krispowell@avocacapital.com>
2012-11-01 21:58:00 +0000

    Commit topfunky files locally, also b/c they have changed

--- .zsh/git-cwd-info.rb
+++ .zsh/git-cwd-info.rb
@@ -1,1 +1,65 @@
-/Users/kip/code/zsh-simple/bin/git-cwd-info.rb
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+# Emits Git metadata for use in a Zsh prompt.
+#
+# AUTHOR:
+#    Ben Hoskings
+#   https://github.com/benhoskings/dot-files/blob/master/files/bin/git_cwd_info
+#
+# MODIFIED:
+#    Geoffrey Grosenbach http://peepcode.com
+
+# The methods that get called more than once are memoized.
+
+def git_repo_path
+  @git_repo_path ||= `git rev-parse --git-dir 2>/dev/null`.strip
+end
+
+def in_git_repo
+  !git_repo_path.empty? &&
+  git_repo_path != '~' &&
+  git_repo_path != "#{ENV['HOME']}/.git"
+end
+
+def git_parse_branch
+  @git_parse_branch ||= `~/.zsh/git-current-branch`.chomp
+end
+
+def git_head_commit_id
+  `git rev-parse --short HEAD 2>/dev/null`.strip
+end
+
+def git_cwd_dirty
+  " %{\e[90m%}✗%{\e[0m%}" unless git_repo_path == '.' || `git ls-files -m`.strip.empty?
+end
+
+def rebasing_etc
+  if File.exists?(File.join(git_repo_path, 'BISECT_LOG'))
+    "+bisect"
+  elsif File.exists?(File.join(git_repo_path, 'MERGE_HEAD'))
+    "+merge"
+  elsif %w[rebase rebase-apply rebase-merge ../.dotest].any? {|d| File.exists?(File.join(git_repo_path, d)) }
+    "+rebase"
+  end
+end
+
+def clean? 
+  @uncommitted ||= `git status --short`.empty?
+end
+
+
+def git_mins_since_commit
+  "#{mins_since_commit}m" unless clean? 
+end
+
+def mins_since_commit
+  now=`date +%s`.to_i
+  last_commit_time=`git log --pretty=format:'%at' -1 2>/dev/null`.to_i
+  secs_since_commit = ( now - last_commit_time )
+  secs_since_commit / 60
+end
+
+if in_git_repo
+  print " %{\e[90m%}#{git_parse_branch} %{\e[37m%}#{git_head_commit_id}%{\e[0m%}#{rebasing_etc}#{git_cwd_dirty} #{git_mins_since_commit}"
+end
