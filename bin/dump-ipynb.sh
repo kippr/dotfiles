68fe812104ae1d24e725b8b11b230745306de714
Kris Powell <kris.powell@kkr.com>
2020-06-12 12:19:04 +0100

    tweak to improve git diff

--- bin/dump-ipynb.sh
+++ bin/dump-ipynb.sh
@@ -3,6 +3,7 @@
 jq --indent 1 \
     '
     (.cells[] | select(has("execution_count")) | .execution_count) = null
+    | (.cells[] | select(has("metadata")) | .metadata) = {}
     | .metadata = {"language_info": {"name":"python", "pygments_lexer": "ipython3"}}
     | (.cells[] | select(has("outputs")) | .outputs[0]["data"]["text/html"]) = []
     | (.cells[] | select(has("outputs")) | .outputs[0]["execution_count"]) = null
